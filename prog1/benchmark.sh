030173c7268d64cd41b5d74d4dd0621a704a32a1
João Fonseca <joao.fonseca@ua.pt>
2024-05-31 15:22:32 +0100

    update benchmark sh

--- prog1/benchmark.sh
+++ prog1/benchmark.sh
@@ -1,18 +1,19 @@
 # Usage: ./benchmark.sh
 # Description: Compiles the source code, runs the cuda-based bitonic sort by rows program, and outputs the results in a
-#              "results" folders, for each configuration of threads (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024).
+#              "results.csv" file, for each configuration of threads (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024).
 # Example: ./benchmark.sh
 
-OUTPUT_FOLDER="results"
+OUTPUT_FILE="results.csv"
 NUMBERS_FOLDER="data"
 
 N_THREADS="1 2 4 8 16 32 64 128 256 512 1024"
-N_ITERATIONS=1
+N_ITERATIONS=15
 
-# Create the output folder
-mkdir -p $OUTPUT_FOLDER
-rm -f $OUTPUT_FOLDER/all.txt
-touch $OUTPUT_FOLDER/all.txt
+# Create the output file
+rm -f $OUTPUT_FILE
+touch $OUTPUT_FILE
+
+echo "k,t1,t2,t3" > $OUTPUT_FILE
 
 # Compile the source code
 nvcc -O2 -Wno-deprecated-gpu-targets -o bmprog1 cudaBitonicRows.cu
@@ -20,17 +21,11 @@ nvcc -O2 -Wno-deprecated-gpu-targets -o bmprog1 cudaBitonicRows.cu
 # Run the program for each configuration of processes and array sizes
 for threads in $N_THREADS; do
   echo "Running program $N_ITERATIONS times for $threads threads..."
-  # Create the output file
-  OUTPUT_FILE="$OUTPUT_FOLDER/t$threads.txt"
-  rm -f $OUTPUT_FILE
-  touch $OUTPUT_FILE
-  echo '--- Results for' $threads 'threads' > $OUTPUT_FILE
   # Run the program and save the results
   for i in $(seq 1 $N_ITERATIONS); do
-    ./bmprog1 -f $NUMBERS_FOLDER/datSeq1M.bin -k $threads | grep -Po '[0-9]+\.[0-9]+e[+-][0-9]+(?= seconds)' >> $OUTPUT_FILE
+    times=$(./bmprog1 -f $NUMBERS_FOLDER/datSeq1M.bin -k $threads | grep -Po '[0-9]+\.[0-9]+e[+-][0-9]+(?= seconds)' | paste -sd ',')
+    echo "$threads,$times" >> $OUTPUT_FILE
   done
-  # Combine all the results into a single file
-  cat $OUTPUT_FILE >> $OUTPUT_FOLDER/all.txt
 done
 
 # Clean-up
